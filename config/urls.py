25c5c81d38516ab4c222b61e2b5bee79ae77bfbd
Julian Kim <juliankim4321@gmail.com>
2026-01-19 04:56:34 -0500

    Fix import order in URL configuration for consistency

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from links.views import create_link, redirect_link
+from django.views.generic import TemplateView
 from django.http import HttpResponse
+
+from links.views import create_link, redirect_link
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="index.html"), name="home"),
