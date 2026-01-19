9f0777ef46484fe28e13524d9d114f3a92319191
Julian Kim <juliankim4321@gmail.com>
2026-01-19 03:06:18 -0500

    Update static file storage configuration and fix release command syntax in fly.toml

--- config/settings.py
+++ config/settings.py
@@ -130,6 +130,8 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 STORAGES = {
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
